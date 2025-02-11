703fdc0a97ffefa00837e8bd9bc8e8a16bf6e6f9
kevintprivett <kevintprivett@gmail.com>
2025-02-11 22:27:15 +0000

    starts 3a exercises

---
+++ part3/phonebook/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+
+let persons = 
+  [
+    {
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+  ]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello, World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) => {
+  const todayDate = new Date(Date.now())
+  const today = todayDate.toUTCString()
+
+  response.send(`
+    <div>Phonebook has info for ${persons.length} people</div>
+    <br />
+    <div>${today}</div>
+  `)
+})
+
+const PORT = 5173
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
